fdc8d6d4618532b1117269c13dc68d9e020b89f1
enrico <enrico.marquez@duko.com.br>
2024-11-14 17:01:07 -0300

    add information about the db into payload

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,18 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log("TESTE", result.rows);
-  response.status(200).json({ chave: "valor" });
+  const version = await database.query("SELECT version();");
+  const max = await database.query("SHOW max_connections;");
+  const opened = await database.query("SELECT COUNT(*) FROM pg_stat_activity;");
+  const updatedAt = new Date().toISOString();
+  response.status(200).json({
+    updated_at: updatedAt,
+    database: {
+      version: version.rows[0].version,
+      max_connections: max.rows[0].max_connections,
+      opened_connections: opened.rows[0].count,
+    },
+  });
 }
 
 export default status;
