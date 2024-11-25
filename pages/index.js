fbfbe9e5e734af43e4c1824218dcfe0818495197
Enrico Marquez <124639774+Enric0Mz@users.noreply.github.com>
2024-11-25 21:35:43 +0000

    fix eslint linting

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,4 @@ function Home() {
   return <h1>Marceline, Juan eu amo vcs. Se voce me ama, da uma risadinha</h1>;
 }
 
-function teste() {
-  console.log("teste");
-}
-
-function teste2() {
-  console.log("identacao errada");
-}
-
 export default Home;
