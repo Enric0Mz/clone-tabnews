bc82412c4334e290870c6363109bc249c104d768
Enrico Marquez <124639774+Enric0Mz@users.noreply.github.com>
2024-11-28 21:39:27 +0000

    feat: implement `Database` in status page

--- pages/status/index.js
+++ pages/status/index.js
@@ -11,6 +11,7 @@ export default function statusPage() {
     <>
       <h1>Status</h1>
       <UpdatedAt />
+      <Database />
     </>
   );
 }
@@ -19,11 +20,29 @@ function UpdatedAt() {
   const { isLoading, data } = useSWR("/api/v1/status", fetchAPI, {
     refreshInterval: 2000,
   });
-  let updatedAtText = "Carregando...";
+  let updatedAtText = loadingValue();
 
   if (!isLoading && data) {
     updatedAtText = new Date(data.updated_at).toLocaleString("pt-BR");
   }
 
   return <div>Última atualização: {updatedAtText}</div>;
+}
+
+function Database() {
+  const { isLoading, data } = useSWR("/api/v1/status", fetchAPI, {
+    refreshInterval: 2000,
+  });
+
+  let databaseText = loadingValue();
+
+  if (!isLoading && data) {
+    databaseText = `Opened connections: ${data.dependencies.database.opened_connections} Version: ${data.dependencies.database.version} Max Connections: ${data.dependencies.database.max_connections}`;
+
+    return <div>Database Information: {databaseText}</div>;
+  }
+}
+
+function loadingValue() {
+  return "Carregando...";
 }
