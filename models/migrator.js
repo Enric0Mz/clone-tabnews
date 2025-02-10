d7ab977fc8cac94bbb1ced64ec0e447c04d1d9b8
Enrico Marquez <124639774+Enric0Mz@users.noreply.github.com>
2025-02-10 20:09:55 +0000

    feat: implement `migrator` model

---
+++ models/migrator.js
@@ -0,0 +1,45 @@
+import migrationRunner from "node-pg-migrate";
+import { resolve } from "node:path";
+import database from "infra/database.js";
+
+const migrationOptions = {
+  dryRun: true,
+  dir: resolve("infra", "migrations"),
+  direction: "up",
+  verbose: true,
+  migrationsTable: "pgmigrations",
+};
+
+async function listPendingMigrations() {
+  let dbClient = await database.getNewClient();
+  try {
+    const pendingMigrations = await migrationRunner({
+      ...migrationOptions,
+      dbClient: dbClient,
+    });
+    return pendingMigrations;
+  } finally {
+    await dbClient?.end();
+  }
+}
+
+async function runPendingMigrations() {
+  let dbClient = await database.getNewClient();
+  try {
+    const migratedMigrations = await migrationRunner({
+      ...migrationOptions,
+      dryRun: false,
+      dbClient: dbClient,
+    });
+    return migratedMigrations;
+  } finally {
+    await dbClient?.end();
+  }
+}
+
+const migrator = {
+  listPendingMigrations,
+  runPendingMigrations,
+};
+
+export default migrator;
